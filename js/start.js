183ffa30adcfc6fab212f478ded5fd35161e9856
zhzkal2 <64739786+zhzkal2@users.noreply.github.com>
2023-07-05 23:51:14 +0900

    Add files via upload

---
+++ js/start.js
@@ -0,0 +1,121 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+const result = document.querySelector("#result")
+
+const end = 12;
+const select = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+function calResult(){
+    var result = select.indexOf(Math.max(...select));
+    return result;
+
+
+    
+}
+
+function setResult(){
+    let point = calResult();
+    const resultName = document.querySelector('.resultname');
+    resultName.innerHTML = infoList[point].name;
+
+    var resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+    var imgURL = 'img/image-' + point + '.png';
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+    imgDiv.appendChild(resultImg);
+
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
+
+
+}
+
+function resultNext(){
+
+    qna.style.WebkitAnimation = "fadeOut 1s";
+    qna.style.Animation = "fadeOut 1s";
+    setTimeout(() => {
+        result.style.WebkitAnimation = "fadeIn 1s";
+        result.style.Animation = "fadeIn 1s";
+        setTimeout(() => {
+            qna.style.display = "none";
+            result.style.display = "block";
+        }, 450)
+
+    },450);
+    setResult();
+
+}
+
+function addAnswer(answerText, qIdx, idx){
+    var a = document.querySelector('.answerBox');
+    var answer = document.createElement('button');
+    answer.classList.add('answerList');
+    answer.classList.add('my-3');
+    answer.classList.add('py-3');
+    answer.classList.add('mx-auto');
+    answer.classList.add('fadeIn');
+
+    a.appendChild(answer);
+    answer.innerHTML = answerText;
+
+    answer.addEventListener("click", function(){
+        var children = document.querySelectorAll('.answerList');
+        for(let i=0; i<children.length; i++){
+            children[i].disabled = true;
+            children[i].style.WebkitAnimation = "fadeOut 0.5s";
+            children[i].style.Animation = "fadeOut 0.5s";
+        }
+
+        setTimeout(() =>{
+
+            var target =  qnaList[qIdx].a[idx].type;
+            for(let i =0; i<target.length; i++){
+                select[target[i]]+=1;
+            }
+
+            for(let i=0; i<children.length; i++){
+                children[i].style.display = 'none';
+            }
+            Next(++qIdx) 
+        },450)
+        
+        },false);
+    }
+
+function Next(qIdx){
+    if(qIdx === end ){
+        resultNext();
+    }
+
+    var q = document.querySelector('.qBox');
+    q.innerHTML = qnaList[qIdx].q;
+
+    for(let i in qnaList[qIdx].a){
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx ,i);
+
+    }
+    var status = document.querySelector('.statusBar');
+    status.style.width = ((100/end)*(qIdx+1)+'%');
+
+}
+function begin(){
+    main.style.WebkitAnimation = "fadeOut 1s";
+    main.style.Animation = "fadeOut 1s";
+    setTimeout(() => {
+        qna.style.WebkitAnimation = "fadeIn 1s";
+        qna.style.Animation = "fadeIn 1s";
+        setTimeout(() => {
+            main.style.display = "none";
+            qna.style.display = "block";
+        }, 450)
+        let qIdx = 0;
+        Next(qIdx);
+
+
+    },450);
+    // main.style.display = "none";
+    // qna.style.display = "block";
+}
